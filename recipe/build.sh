8ab272b6bb71847c222071c3e80a05219bf549b8
Paul McCarthy <pauldmccarthy@gmail.com>
2022-05-31 18:16:44 +0100

    MNT: glib-networking is needed for one test, and giomm_simple_test will fail unless a file called /etc/fstab exists

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 set -ex
 
+# The giom_simple_test unit test will fail
+# if /etc/fstab does not exist.
+#
+# Meson does not have the ability to skip
+# tests (https://github.com/mesonbuild/meson/issues/6999),
+# so in order to skip this test, we remove
+# it from the build config
+sed -i.bk "s/^.*giomm_simple.*$//g" tests/meson.build
+
+rm -r tests/giomm_simple
+
+
 meson setup                \
       --prefix="${PREFIX}" \
       --buildtype=release  \
       ./builddir .
 
 cd builddir
+
 ninja
 ninja install
 ninja test
