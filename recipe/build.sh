92201af081355fb787bbadc1f05385d31651a685
Paul McCarthy <pauldmccarthy@gmail.com>
2022-05-31 17:41:22 +0100

    MNT,RF: Bump to 2.70.0, use meson/ninja instead of autotools for build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 set -ex
 
-# configure, make, install, check
-sed -e '/^libdocdir =/ s/$(book_name)/glibmm-'"${PKG_VERSION}"'/' \
-    docs/Makefile.in > docs/Makefile.in.new
-mv docs/Makefile.in.new docs/Makefile.in
+meson setup                \
+      --prefix="${PREFIX}" \
+      --buildtype=release  \
+      ./builddir .
 
-./configure --prefix="${PREFIX}" --exec-prefix="${PREFIX}" \
-  --disable-dependency-tracking \
-  || { cat config.log; exit 1; }
-
-make -j${CPU_COUNT}
-make install
+cd builddir
+ninja
+ninja install
+ninja test
