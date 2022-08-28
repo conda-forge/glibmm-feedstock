b8d0b7ab3e50ee3a4163d6c361f528c69c83476e
Tom Schoonjans <Tom.Schoonjans@genomicsplc.com>
2022-08-28 11:19:18 +0530

    Bump to latest 2.66.x release, refactor, add windows build

---
+++ recipe/build_glibmm.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -ex
+
+# get meson to find pkg-config when cross compiling
+export PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config
+
+meson ${MESON_ARGS} --wrap-mode=nofallback --buildtype=release --prefix="${PREFIX}" -Dlibdir=lib builddir .
+
+ninja -C builddir -j${CPU_COUNT}
+ninja -C builddir install
